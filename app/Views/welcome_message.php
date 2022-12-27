8920af841610d8da235e03a9d67712c1737a3ca0
Raju <rsingh.rks60@gmail.com>
2022-12-27 13:43:30 +0530

    initial commit

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome to CodeIgniter 4!</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+
+    <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>    
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col"><input type="text" id="a1" ></div>
+            <div class="col"><input type="text" id="a2" ></div>
+            <div class="col"><input type="text" id="total" disabled></div>
+        </div>
+        
+    </div>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            var total = 0;
+            $('#a1,#a2').keyup(function(){
+                var b1 = $('#a1').val();
+                var b2 = $('#a2').val();
+                var total = b1*b2;
+                $('#total').val(total);
+            });
+        });
+    </script>
+</body>
+</html>
